97186f2462cf881e7e6675e952d83fd094f7cea2
ethanh2003 <echerring.ech@gmail.com>
2023-10-05 20:43:03 -0400

    fixed import

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Resume from './Resume';
 import Contact from './Contact';
 
 const App = () => {
     return (
         <Router>
-            <Switch>
+            <Routes>
                 <Route exact path="/" component={Resume} />
                 <Route exact path="/contact" component={Contact} />
 
-            </Switch>
+            </Routes>
         </Router>
     );
 };
